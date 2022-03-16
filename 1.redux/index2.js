af6ed56856427a04f24d134867244159e71dd186
정석우 <ingal777@naver.com>
2022-03-16 19:48:25 +0900

    redux 예제에 적용하기

--- 1.redux/index2.js
+++ 1.redux/index2.js
@@ -1,19 +1,22 @@
 const { createStore } = require('redux');
 
-const reducer = (prevState, action) => { // 새로운 state 만들어주기
+const reducer = (prevState, action) => { //리듀서:액션을 받아서 새로운 state를 만들어주는함수(이전상태,액션) //액션과 리듀서는 짝임
   switch (action.type) {
     case 'LOG_IN':
       return {
-        ...prevState,
+        ...prevState, //바뀌지않는건 유지하면서,바뀌는부분만 적용
         user: action.data,
+        isLoggingIn:true
       };
     case 'LOG_OUT':
       return {
         ...prevState,
         user: null,
+        isLoggingIn:false
       };
     case 'ADD_POST':
       return {
+        ...prevState,
         posts: [...prevState.posts, action.data],
       };
     default:
@@ -23,7 +26,7 @@ const reducer = (prevState, action) => { // 새로운 state 만들어주기
 
 const initialState = {
   user: null,
-  isLoggingIn: true,
+  isLoggingIn: false,
   posts: [],
   comments: [],
   favorites: [],
@@ -31,23 +34,22 @@ const initialState = {
   likes: [],
   followers: [],
 };
-
-const store = createStore(reducer, initialState);
-store.subscribe(() => { // react-redux 안에 들어있어요.
-  console.log('changed'); // 화면 바꿔주는 코드 여기서
-});
-
+const store = createStore(reducer, initialState); //스토어생성 (리듀서,초기state)
 console.log('1st', store.getState());
 
+
+
+//action생성
+
 const logIn = (data) => {
-  return { // action
+  return { 
     type: 'LOG_IN',
     data,
   };
 };
 
 const logOut = () => {
-  return { // action
+  return { 
     type: 'LOG_OUT',
   };
 };
@@ -59,7 +61,7 @@ const addPost = (data) => {
   }
 };
 
-// --------------------------------------
+//dispatch 위에부분은 미리정의 , 아랫부분은 필요에따라 쓰기
 
 store.dispatch({
   type: 'LOG_IN_REQUEST',
@@ -67,7 +69,7 @@ store.dispatch({
 
 store.dispatch(logIn({
   id: 1,
-  name: 'zerocho',
+  name: 'jung',
   admin: true,
 }));
 console.log('2nd', store.getState());
@@ -78,6 +80,7 @@ store.dispatch(addPost({
   content: '안녕하세요. 리덕스',
 }));
 console.log('3rd', store.getState());
+
 store.dispatch(addPost({
   userId: 1,
   id: 2,
